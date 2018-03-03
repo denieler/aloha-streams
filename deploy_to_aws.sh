3e1669654340b93cfa7c07d3284829b06115838c
Daniel Ostapenko <danielostapenko@gmail.com>
2018-03-03 18:07:47 +0100

    fix: return back --force when ecs-cli create container

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -7,6 +7,6 @@ ecs-cli configure --region $AWS_DEFAULT_REGION --cluster $AWS_ECS_CLUSTER_NAME
 # ecs-cli ps --cluster $AWS_ECS_CLUSTER_NAME --region $AWS_DEFAULT_REGION --ecs-profile $PROFILE_NAME
 
 #start cluster
-ecs-cli up --cluster $AWS_ECS_CLUSTER_NAME --verbose -region $AWS_DEFAULT_REGION --instance-role $ROLE_NAME --size 1 --instance-type t2.micro --vpc $VPC_ID --subnets $SUBNET_ID --security-group $SECURITY_GROUP_ID --ecs-profile $PROFILE_NAME
+ecs-cli up --cluster $AWS_ECS_CLUSTER_NAME --verbose -region $AWS_DEFAULT_REGION --instance-role $ROLE_NAME --size 1 --instance-type t2.micro --vpc $VPC_ID --subnets $SUBNET_ID --security-group $SECURITY_GROUP_ID --ecs-profile $PROFILE_NAME --force
 #run docker compose over clusters
 ecs-cli compose service up --cluster $AWS_ECS_CLUSTER_NAME --region $AWS_DEFAULT_REGION --ecs-profile $PROFILE_NAME --timeout 20
