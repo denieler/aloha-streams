b2c7a954edca298859f0cd6b440899fa93c3d209
Daniel Ostapenko <danielostapenko@gmail.com>
2018-03-03 16:26:50 +0100

    feat: add deploy process for Circle CI and AWS ECS

---
+++ deploy_to_aws.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ecs-cli configure profile --profile-name $PROFILE_NAME --access-key $AWS_ACCESS_KEY_ID --secret-key $AWS_SECRET_ACCESS_KEY
+ecs-cli configure --region $AWS_DEFAULT_REGION --cluster $AWS_ECS_CLUSTER_NAME
+
+#status of the cluster
+# ecs-cli ps --cluster $AWS_ECS_CLUSTER_NAME --region $AWS_DEFAULT_REGION --ecs-profile $PROFILE_NAME
+
+#start cluster
+ecs-cli up --cluster $AWS_ECS_CLUSTER_NAME --verbose -region $AWS_DEFAULT_REGION --instance-role $ROLE_NAME --size 1 --instance-type t2.micro --vpc $VPC_ID --subnets $SUBNET_ID --security-group $SECURITY_GROUP_ID --ecs-profile $PROFILE_NAME --force
+#run docker compose over clusters
+ecs-cli compose service up --cluster $AWS_ECS_CLUSTER_NAME --region $AWS_DEFAULT_REGION --ecs-profile $PROFILE_NAME --timeout 20
--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@ const Hapi = require('hapi')
 
 // Create a server with a host and port
 const server = Hapi.server({ 
-    host: 'localhost', 
+    host: '0.0.0.0', 
     port: 3000 
 })
 
