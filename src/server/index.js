cfe8f5855e72d094ae46299ffe05acbd42fc9937
Daniel Ostapenko <danielostapenko@gmail.com>
2018-03-29 22:59:00 +0200

    fix: add linting for site and server projects

--- src/server/index.js
+++ src/server/index.js
@@ -1,32 +1,32 @@
 const Hapi = require('hapi')
 
 // Create a server with a host and port
-const server = Hapi.server({ 
-    host: '0.0.0.0', 
-    port: 8083 
+const server = Hapi.server({
+  host: '0.0.0.0',
+  port: 8083
 })
 
 // Add the route
 server.route({
-    method: 'GET',
-    path:'/hello', 
-    handler: function (request, h) {
-        return 'hello world'
-    }
+  method: 'GET',
+  path: '/hello',
+  handler: function (request, h) {
+    return 'hello world'
+  }
 })
 
 // Start the server
-async function start() {
+async function start () {
+  try {
+    await server.start()
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log(err)
+    process.exit(1)
+  }
 
-    try {
-        await server.start()
-    }
-    catch (err) {
-        console.log(err)
-        process.exit(1)
-    }
-
-    console.log('Server running at:', server.info.uri)
+  // eslint-disable-next-line no-console
+  console.log('Server running at:', server.info.uri)
 }
 
 start()
