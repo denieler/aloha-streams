a26f46e417c4b0ece26edd59b4f614ed5a0dfa72
Daniel Ostapenko <danielostapenko@gmail.com>
2018-04-21 10:22:11 +0200

    fix: add mobile burger icon, replaced fontawesome with the newer version

--- src/site/public/js/main.js
+++ src/site/public/js/main.js
@@ -4,6 +4,15 @@ document.addEventListener('DOMContentLoaded', function () {
   // eslint-disable-next-line
   new ClipboardJS('.btn')
 
+  const toggleMobileMenu = (e) => {
+    const mobileMenu = document.getElementsByClassName('header__buttons')
+    if (mobileMenu && mobileMenu.length) {
+      mobileMenu[0].classList.toggle('header__buttons--show')
+    }
+  }
+  const headerMenuBurgerLink = document.getElementById('header-buttons-mobile')
+  headerMenuBurgerLink.addEventListener('click', toggleMobileMenu)
+
   // /challenges/mine
 
   const onChallengeAccepted = (e) => {
